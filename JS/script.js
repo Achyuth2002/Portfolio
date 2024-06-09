5f7d9b1858c0847b339892aa79f8c9ace8a23b36
achyuth-anil <anilkumarachyuth3@gmail.com>
2024-06-09 22:41:02 +0530

    On branch master  Changes to be committed: 	deleted:    CSS/all.min.css 	deleted:    CSS/bootstrap.min.css 	deleted:    CSS/fonts.css 	deleted:    CSS/main.css 	new file:   CSS/media.css 	deleted:    CSS/partials/global.css 	deleted:    CSS/partials/variables.css 	deleted:    CSS/responsive.css 	modified:   CSS/style.css 	deleted:    JS/bootstrap.min.js 	deleted:    JS/jquery.3.5.1.js 	deleted:    JS/main.js 	new file:   JS/script.js 	new file:   assets/Achyuth-Resume.pdf 	renamed:    img/lotus-pr.jpeg -> assets/about-pic.jpeg 	new file:   assets/ai.png 	new file:   assets/aoi-icons.zip 	new file:   assets/arrow-down.png 	new file:   assets/arrow-up.png 	renamed:    img/bg.png -> assets/bg.png 	renamed:    img/bg1.png -> assets/bg1.png 	new file:   assets/checkmark (1).png 	new file:   assets/checkmark.png 	new file:   assets/cloud-computing.png 	renamed:    img/IMG_20211128_082042_750.jpg -> assets/contact-pic.jpg 	renamed:    img/dark.png -> assets/dark.png 	new file:   assets/data-analysis.png 	new file:   assets/education.png 	new file:   assets/email.png 	new file:   assets/experience.png 	new file:   assets/favicon.ico 	new file:   assets/github.png 	renamed:    img/hi.png -> assets/hi.png 	new file:   assets/insta.png 	new file:   assets/linkedin.png 	renamed:    img/mail.png -> assets/mail.png 	new file:   assets/profile-pic.jpeg 	new file:   assets/project-1.png 	new file:   assets/project-2.png 	new file:   assets/project-3.png 	new file:   assets/skills.png 	new file:   assets/web.png     deleted:    fonts/static/Cabin-Bold.ttf 	deleted:    fonts/static/Cabin-BoldItalic.ttf 	deleted:    fonts/static/Cabin-Italic.ttf 	deleted:    fonts/static/Cabin-Medium.ttf 	deleted:    fonts/static/Cabin-MediumItalic.ttf 	deleted:    fonts/static/Cabin-Regular.ttf     deleted:    fonts/static/Cabin-SemiBold.ttf 	deleted:    fonts/static/Cabin-SemiBoldItalic.ttf 	deleted:    fonts/static/Cabin_Condensed-Bold.ttf 	deleted:    fonts/static/Cabin_Condensed-BoldItalic.ttf 	deleted:    fonts/static/Cabin_Condensed-Italic.ttf 	deleted:    fonts/static/Cabin_Condensed-Medium.ttf 	deleted:    fonts/static/Cabin_Condensed-MediumItalic.ttf 	deleted:    fonts/static/Cabin_Condensed-Regular.ttf 	deleted:    fonts/static/Cabin_Condensed-SemiBold.ttf 	deleted:    fonts/static/Cabin_Condensed-SemiBoldItalic.ttf 	deleted:    fonts/static/Cabin_SemiCondensed-Bold.ttf 	deleted:    fonts/static/Cabin_SemiCondensed-BoldItalic.ttf 	deleted:    fonts/static/Cabin_SemiCondensed-Italic.ttf 	deleted:    fonts/static/Cabin_SemiCondensed-Medium.ttf 	deleted:    fonts/static/Cabin_SemiCondensed-MediumItalic.ttf 	deleted:    fonts/static/Cabin_SemiCondensed-Regular.ttf 	deleted:    fonts/static/Cabin_SemiCondensed-SemiBold.ttf 	deleted:    fonts/static/Cabin_SemiCondensed-SemiBoldItalic.ttf 	deleted:    icons/fa-brands-400.eot 	deleted:    icons/fa-brands-400.svg     deleted:    icons/fa-brands-400.ttf 	deleted:    icons/fa-brands-400.woff 	deleted:    icons/fa-brands-400.woff2 	deleted:    icons/fa-regular-400.eot 	deleted:    icons/fa-regular-400.svg 	deleted:    icons/fa-regular-400.ttf 	deleted:    icons/fa-regular-400.woff 	deleted:    icons/fa-regular-400.woff2 	deleted:    icons/fa-solid-900.eot 	deleted:    icons/fa-solid-900.svg 	deleted:    icons/fa-solid-900.ttf 	deleted:    icons/fa-solid-900.woff 	deleted:    icons/fa-solid-900.woff2 	modified:   index.html

---
+++ JS/script.js
@@ -0,0 +1,38 @@
+
+var content = ["Frontend Developer", "Engineering Student"];
+var index = 0;
+var isDeleting = false;
+var txt = '';
+
+function typeWriter() {
+  var speed = isDeleting ? 100 : 200; // Speed for deleting and typing
+  var id = "typing-animation";
+  var target = document.getElementById(id);
+
+  if (!isDeleting && txt.length == content[index].length) {
+    isDeleting = true;
+    setTimeout(typeWriter, 2000); // Wait for 2 seconds before start deleting
+  } else if (isDeleting && txt.length == 0) {
+    isDeleting = false;
+    index = index ^ 1; // Toggle between 0 and 1
+    setTimeout(typeWriter, 500); // Wait for 0.5 seconds before start typing
+  } else {
+    txt = isDeleting
+      ? content[index].substring(0, txt.length - 1)
+      : content[index].substring(0, txt.length + 1);
+    target.textContent = txt;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = function() {
+  typeWriter();
+};
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
